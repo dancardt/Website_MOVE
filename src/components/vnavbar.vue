b115c5cc1750605d8fd79404e3c08a0a17df38ea
DANIEL BALBINOTTE <88980676+dancardt@users.noreply.github.com>
2021-09-09 14:24:55 +0000

    add logo + botão hover serviços

--- src/components/vnavbar.vue
+++ src/components/vnavbar.vue
@@ -3,17 +3,40 @@
         <template #brand>
             <b-navbar-item tag="router-link" :to="{ path: '/' }">
                 <img
-                    src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
+                    src="@/assets/move.png"
                     alt="Lightweight UI components for Vue.js based on Bulma"
+                    style="height: 150px; max-height: none"
                 >
             </b-navbar-item>
         </template>
         <template #start>
+
             <b-navbar-item href="#/">
-                Home
             </b-navbar-item>
-            <b-navbar-item href="#/contato">
-                Contato
+            
+           <b-navbar-item href="#/serviços">
+              <b-dropdown :triggers="['hover']" aria-role="list">
+            <template #trigger>
+                <b-button
+                    label="Serviços"
+                    type="is-info" outlined
+                    icon-right="menu-down" />
+            </template>
+            <b-dropdown-item aria-role="listitem">Venda seu classificado sem sair de casa</b-dropdown-item>
+            <b-dropdown-item aria-role="listitem">Lojistas e marketplaces</b-dropdown-item>
+            <b-dropdown-item aria-role="listitem">Transporte de Doações</b-dropdown-item>
+            <b-dropdown-item aria-role="listitem">Self storages</b-dropdown-item>
+        </b-dropdown>
+            </b-navbar-item>
+
+            <b-navbar-item href="#/parceiros">
+                Parceiros
+            </b-navbar-item>
+
+
+
+            <b-navbar-item href="#/sobre">
+                Sobre a Move
             </b-navbar-item>
         </template>
     </b-navbar>
